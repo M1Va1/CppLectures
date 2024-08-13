43b5cf5192683d4c0b812645625ebb4a3df55f98
nook0110 <nik.blokhtin@gmail.com>
2024-08-13 13:00:56 +0300

    add asserr

--- template/homework_pair/pair.cpp
+++ template/homework_pair/pair.cpp
@@ -3,17 +3,18 @@
 
 class Pair {
 public:
-
-    void display() const {
-    }
+  void display() const {}
 };
 
 int main() {
-    Pair<int, std::string> p(1, "example");
-    p.display(); // Output: First: 1, Second: example
+  Pair<int, std::string> p(1, "example");
+  p.display(); // Output: First: 1, Second: example
 
-    Pair<double, char> p2(3.14, 'P');
-    p2.display(); // Output: First: 3.14, Second: A
+  assert(p.first == 1);
+  assert(p.second == "example");
 
-    return 0;
+  Pair<double, char> p2(3.14, 'P');
+  p2.display(); // Output: First: 3.14, Second: A
+
+  return 0;
 }
